db4586e758e167a67b900b8d2c58025f09bfc11b
Toby Davis <pencilcaseman@gmail.com>
2025-05-17 20:18:36 +0100

    Updates

--- ghostty/ghostty_startup.sh
+++ ghostty/ghostty_startup.sh
@@ -4,6 +4,11 @@
 export PATH=$HOME/.nix-profile/bin:$PATH
 export PATH=$HOME/.cargo/bin:$PATH
 
+# If fish exists, set the shell to that
+if which fish >/dev/null 2>&1; then
+	export SHELL=fish
+fi
+
 zellij && exit
 
 # # Try fish with tmux
