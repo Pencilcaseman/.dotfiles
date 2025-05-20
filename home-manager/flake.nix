a68270f1c182cf09dc09895904f4b84633179b24
Toby Davis <pencilcaseman@gmail.com>
2025-05-20 17:31:17 +0100

    Update nix stuff

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -4,10 +4,21 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     # nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-24.05-darwin";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
+
+  nixConfig =  {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 
   outputs = { nixpkgs, home-manager, ... }: {
